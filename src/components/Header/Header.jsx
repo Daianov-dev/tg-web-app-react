1b9e07c485c027157721e51a5821616612011131
Daianov-dev <dinardaian@gmail.com>
2024-04-15 20:07:40 +0300

    fixed Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,12 +3,12 @@ import { useTelegram } from '../../hooks/useTelegram';
 import Button from '../Button/Button';
 
 const Header = (props) => {
-  const { tg, user, onClose } = useTelegram();
+  const { user, onClose } = useTelegram();
 
   return (
     <div className={'header'}>
       <Button onClick={onClose}>Закрыть</Button>
-      <span className={'username'}>{tg.initDataUnsafe?.user?.username}</span>
+      <span className={'username'}>{user?.username}</span>
     </div>
   );
 };
